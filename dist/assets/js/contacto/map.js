7bd7737cc358592b021ef3a0f64fde90b2ce6def
unknown <carlos.pagaro@yeyeko-ux.mx>
2020-11-11 22:54:05 -0600

    contacto

--- dist/assets/js/contacto/map.js
+++ dist/assets/js/contacto/map.js
@@ -1,4 +1,23 @@
 
+
+var geojson = {
+    type: 'FeatureCollection',
+    features: [{
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-99.1602889, 19.3767594]
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'SISSA Monitoring Integral'
+      }
+    }]
+  };
+
+
+
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiYmFsY2F6YXJodW0xIiwiYSI6ImNraGQyMXI2aTBlc3QycW1kYWhvYWgxc3QifQ.6ILivHfgoiqLNS_r19-obA';  
 var map = new mapboxgl.Map({ 
     container: 'map', 
@@ -7,6 +26,24 @@ var map = new mapboxgl.Map({
     zoom: 13
 }); 
 
-var marker = new mapboxgl.Marker()
-.setLngLat([-99.1602889, 19.3767594])
-.addTo(map);
+
+
+  // add markers to map
+  geojson.features.forEach(function(marker) {
+
+    // create a HTML element for each feature
+    var el = document.createElement('div');
+    el.className = 'marker';
+  
+    // make a marker for each feature and add to the map
+    new mapboxgl.Marker(el)
+      .setLngLat(marker.geometry.coordinates)
+      .addTo(map);
+  });
+
+
+
+
+// var marker = new mapboxgl.Marker()
+// .setLngLat([-99.1602889, 19.3767594])
+// .addTo(map);
